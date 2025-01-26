35d37e0313cd35bfa47319ee618046028725565f
CHRISSCAHN <christianpaul090402@gmail.com>
2025-01-26 10:52:30 +0800

    check nung sa user na session

--- F01/pages/billing_user.php
+++ F01/pages/billing_user.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include("../connect.php");
-if (!isset($_SESSION['loggedin']) || !isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['loggedin']) || !isset($_SESSION['id'])) {
   header("Location: sign-in.php");
   exit();
 }
